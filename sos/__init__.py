73649013298d37630208175c0770ea4f9337fd0d
David Goetz <dpgoetz@gmail.com>
2012-11-27 09:14:56 -0800

    adding stdeb config and upping version

--- sos/__init__.py
+++ sos/__init__.py
@@ -16,7 +16,7 @@
 import gettext
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (1, 1, 5,)
+version_info = (1, 1, 6,)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
 gettext.install('sos')
