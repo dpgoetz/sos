f6defeaea26c676d8c4697cfaefa1d72d7155456
David Goetz <dpgoetz@gmail.com>
2014-02-13 07:29:49 -0800

    upping vrs

--- sos/__init__.py
+++ sos/__init__.py
@@ -16,7 +16,7 @@
 import gettext
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (1, 1, 16,)
+version_info = (1, 1, 17,)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
 gettext.install('sos')
